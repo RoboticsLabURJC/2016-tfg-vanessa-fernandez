e791b1e2a2f2be0aa3b4da51a931dd38ae822987
vmartinezf <vanessa_1895@msn.com>
2016-07-09 11:52:44 +0200

    follow line

--- Practices of robotics/follow_line/MyAlgorithm.py
+++ Practices of robotics/follow_line/MyAlgorithm.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 from sensors import sensor
 import numpy as np
 import threading
@@ -23,35 +26,35 @@ class MyAlgorithm():
         # Add your code here
         print "Runing"
 
-        # Cambiamos de modelo RGB a HSV
+        # RGB model change to HSV
         imageRight_HSV = cv2.cvtColor(imageRight, cv2.COLOR_RGB2HSV)
         imageLeft_HSV = cv2.cvtColor(imageLeft, cv2.COLOR_RGB2HSV)
 
-        # Minimo y maximo valores del rojo
+        # Minimum and maximum values ​​of the red
         value_min_HSV = np.array([0, 235, 60])
         value_max_HSV = np.array([180, 255, 255])
 
-        # Filtramos las imagenes
+        # Filtering images
         imageRight_HSV_filtered = cv2.inRange(imageRight_HSV, value_min_HSV, value_max_HSV)
         imageLeft_HSV_filtered = cv2.inRange(imageLeft_HSV, value_min_HSV, value_max_HSV)
 
 
-        #Crear una mascara con solo los pixeles dentro del rango de rojos
+        # Creating a mask with only the pixels within the range of red
         imageRight_HSV_filtered_Mask = np.dstack((imageRight_HSV_filtered, imageRight_HSV_filtered, imageRight_HSV_filtered))
         imageLeft_HSV_filtered_Mask = np.dstack((imageLeft_HSV_filtered, imageLeft_HSV_filtered, imageLeft_HSV_filtered))
 
 
-        # Con shape miramos el numero de filas y columnas de una imagen
-        tamano = imageLeft.shape
-        filas = tamano[0]
-        columnas = tamano[1]
+        # Shape gives us the number of rows and columns of an image
+        size = imageLeft.shape
+        rows = size[0]
+        columns = size[1]
 
 
-        # Busqueda de pixeles que cambian de tono
+        #  Looking for pixels that change of tone
         position_pixel_left = []
         position_pixel_right  = []
 
-        for i in range(0, columnas-1):
+        for i in range(0, columns-1):
             value = imageLeft_HSV_filtered[365, i] - imageLeft_HSV_filtered[365, i-1]
             if(value != 0):
                 if (value == 255):
@@ -60,11 +63,11 @@ class MyAlgorithm():
                     position_pixel_right.append(i-1)
 
 
-        # Calculo de la posicion intermedia de la carretera
+        # Calculating the intermediate position of the road
         if ((len(position_pixel_left) != 0) and (len(position_pixel_right) != 0)):
             position_middle = (position_pixel_left[0] + position_pixel_right[0]) / 2
         elif ((len(position_pixel_left) != 0) and (len(position_pixel_right) == 0)):
-            position_middle = (position_pixel_left[0] + columnas) / 2
+            position_middle = (position_pixel_left[0] + columns) / 2
         elif ((len(position_pixel_left) == 0) and (len(position_pixel_right) != 0)):
             position_middle = (0 + position_pixel_right[0]) / 2
         else:
@@ -73,8 +76,8 @@ class MyAlgorithm():
             position_middle = (position_pixel_left[0] + position_pixel_right[0])/ 2
 
 
-        # Calculo de la desviacion
-        desviation = position_middle - (columnas/2)
+        # Calculating the desviation
+        desviation = position_middle - (columns/2)
         print " desviation    ", desviation
 
         #EXAMPLE OF HOW TO SEND INFORMATION TO THE ROBOT ACTUATORS
