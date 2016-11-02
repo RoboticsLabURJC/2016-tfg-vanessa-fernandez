041b69095a2a846cfb9dd81df19974d6c0032d26
vmartinezf <vanessa_1895@msn.com>
2016-11-02 10:31:19 +0100

    pruebas

--- Practices of robotics/global_navigation/MyAlgorithm.py
+++ Practices of robotics/global_navigation/MyAlgorithm.py
@@ -36,76 +36,113 @@ class MyAlgorithm():
 
     def penaltiesObstacles(self, i, j, dest0, dest1):
         mapIm = self.grid.getMap()
-        if (((i == dest0) and (j != dest1)) or ((i != dest0) and (j == dest1))):
-            #if (self.grid.getVal(i, j) < 0.0):
-            #    self.grid.setVal(i, j, 1000.0)
-            #else:
-            if (self.grid.getVal(i, j) >= 0.0):
-                self.grid.setVal(i, j, self.grid.getVal(i, j)+10.0)
-            if ((i == dest0) and (j < dest1)):
-                posx = [i, i]
-                posy = [j-1, j-2]
-            if ((i == dest0) and (j > dest1)):
-                posx = [i, i]
-                posy = [j+1, j+2]
-            if ((i < dest0) and (j == dest1)):
-                posx = [i-1, i-2]
-                posy = [j, j]
-            if ((i > dest0) and (j == dest1)):
-                posx = [i+1, i+2]
-                posy = [j, j]
-            if ((posx[0] >= 0) and (posx[0] < 400) and (posy[0] >= 0) and (posy[0] < 400) and (mapIm[posy[0]][posx[0]] == 255)):
-                #if (self.grid.getVal(posx[0], posy[0]) < 0.0):
-                #    self.grid.setVal(posx[0], posy[0], 1000.0)
-                #else:
-                if (self.grid.getVal(posx[0], posy[0]) >= 0.0):
-                    self.grid.setVal(posx[0], posy[0], (self.grid.getVal(posx[0], posy[0]) + 5.0))
-            if ((posx[1] >= 0) and (posx[1] < 400) and (posy[1] >= 0) and (posy[1] < 400) and (mapIm[posy[1]][posx[1]] == 225)):
-                #if (self.grid.getVal(posx[1], posy[1]) < 0.0):
-                #    self.grid.setVal(posx[1], posy[1], 1000.0)
-                #else:
-                if (self.grid.getVal(posx[1], posy[1]) >= 0.0):
-                    self.grid.setVal(posx[1], posy[1], (self.grid.getVal(posx[1], posy[1]) + 2.0))
-        if ((i != dest0) and (j != dest1)):
-            num_obstacles = 0
-            for t in range(i-1, i+2):
-                for p in range(j-1, j+2):
-                    if ((t >= 0) and (t < 400) and (p >= 0) and (p < 400)):
-                        if (mapIm[p][t] == 0):
-                            num_obstacles = num_obstacles + 1
-            if (num_obstacles == 1):
-                #if (self.grid.getVal(i, j) < 0.0):
-                #    self.grid.setVal(i, j, 1000.0)
-                #else:
-                if (self.grid.getVal(i, j) >= 0.0):
-                    self.grid.setVal(i, j, self.grid.getVal(i, j)+10.0)
-                if ((i < dest0) and (j < dest1)):
-                    posx = [i, i-1, i-1, i, i-1, i-2, i-2, i-2]
-                    posy = [j-1, j, j-1, j-2, j-2, j-2, j-1, j]
-                if ((i < dest0) and (j > dest1)):
-                    posx = [i, i-1, i-1, i, i-1, i-2, i-2, i-2]
-                    posy = [j+1, j, j+1, j+2, j+2, j+2, j+1, j]
-                if ((i > dest0) and (j < dest1)):
-                    posx = [i, i+1, i+1, i, i+1, i+2, i+2, i+2]
-                    posy = [j-1, j, j-1, j-2, j-2, j-2, j-1, j]
-                if ((i > dest0) and (j > dest1)):
-                    posx = [i, i+1, i+1, i, i+1, i+2, i+2, i+2]
-                    posy = [j+1, j, j+1, j+2, j+2, j+2, j+1, j]
-                for u in range(0, len(posx)):
-                    if (u < 3):
-                        if ((posx[u] >= 0) and (posx[u] < 400) and (posy[u] >= 0) and (posy[u] < 400) and (mapIm[posy[u]][posx[u]] == 255)):
-                            #if (self.grid.getVal(posx[u], posy[u]) < 0.0):
-                            #    self.grid.setVal(posx[u], posy[u], 1000.0)
-                            #else:
-                            if (self.grid.getVal(posx[u], posy[u]) >= 0.0):
-                                self.grid.setVal(posx[u], posy[u], (self.grid.getVal(posx[u], posy[u]) + 5.0))
-                    else:
-                        if ((posx[u] >= 0) and (posx[u] < 400) and (posy[u] >= 0) and (posy[u] < 400) and (mapIm[posy[u]][posx[u]] == 255)):
-                            #if (self.grid.getVal(posx[u], posy[u]) < 0.0):
-                            #    self.grid.setVal(posx[u], posy[u], 1000.0)
-                            #else:
-                            if (self.grid.getVal(posx[u], posy[u]) >= 0.0):
-                                self.grid.setVal(posx[u], posy[u], (self.grid.getVal(posx[u], posy[u]) + 2.0))    
+        found = "false"
+        pos = []
+        if ((i == (dest0-1)) or (i == (dest0+1)) or (j == (dest1-1)) or (j == (dest1+1))):
+            penaltie = 10.0
+        elif ((i == (dest0-2)) or (i == (dest0+2)) or (j == (dest1-2)) or (j == (dest1+2))):
+            penaltie = 5.0
+        elif ((i == (dest0-3)) or (i == (dest0+3)) or (j == (dest1-3)) or (j == (dest1+3))):
+            penaltie = 2.0
+
+        for l in range(0, len(self.posPenalties)):
+           if (self.posPenalties[l] == [i, j]):
+                found = "true"
+                pos.append(l)
+        if (found == "false"):
+            self.grid.setVal(i, j, (self.grid.getVal(i, j)+penaltie))
+            self.posPenalties.append([i, j])
+            self.valPenalties.append(penaltie)
+        else:
+            if (penaltie > self.valPenalties[pos[0]]):
+                self.grid.setVal(i, j, (self.grid.getVal(i, j)-self.valPenalties[pos[0]]+penaltie))
+                self.valPenalties[pos[0]] = penaltie
+            
+
+
+    #def penaltiesObstacles(self, i, j, dest0, dest1):
+    #    mapIm = self.grid.getMap()
+    #    if (((i == dest0) and (j != dest1)) or ((i != dest0) and (j == dest1))):
+    #        #if (self.grid.getVal(i, j) < 0.0):
+    #        #    self.grid.setVal(i, j, 1000.0)
+    #        #else:
+    #        self.grid.setVal(i, j, (self.grid.getVal(i, j)+10.0))
+    #        #if (self.grid.getVal(i, j) >= 0.0):
+    #        #    self.grid.setVal(i, j, self.grid.getVal(i, j)+10.0)
+    #        if ((i == dest0) and (j < dest1)):
+    #            posx = [i, i]
+    #            posy = [j-1, j-2]
+    #        if ((i == dest0) and (j > dest1)):
+    #            posx = [i, i]
+    #            posy = [j+1, j+2]
+    #        if ((i < dest0) and (j == dest1)):
+    #            posx = [i-1, i-2]
+    #            posy = [j, j]
+    #        if ((i > dest0) and (j == dest1)):
+    #            posx = [i+1, i+2]
+    #            posy = [j, j]
+    #        if ((posx[0] >= 0) and (posx[0] < 400) and (posy[0] >= 0) and (posy[0] < 400) and (mapIm[posy[0]][posx[0]] == 255)):
+    #            #if (self.grid.getVal(posx[0], posy[0]) < 0.0):
+    #            #    self.grid.setVal(posx[0], posy[0], 1000.0)
+    #            #else:
+    #            #if (self.grid.getVal(posx[0], posy[0]) >= 0.0):
+    #            #    self.grid.setVal(posx[0], posy[0], (self.grid.getVal(posx[0], posy[0]) + 5.0))
+    #            #mapIm[j, i] = 128
+    #            #mapIm[posy[0]][posx[0]] = 128
+    #            self.grid.setVal(posx[0], posy[0], (self.grid.getVal(posx[0], posy[0]) + 5.0))
+    #        if ((posx[1] >= 0) and (posx[1] < 400) and (posy[1] >= 0) and (posy[1] < 400) and (mapIm[posy[1]][posx[1]] == 225)):
+    #            #if (self.grid.getVal(posx[1], posy[1]) < 0.0):
+    #            #    self.grid.setVal(posx[1], posy[1], 1000.0)
+    #            #else:
+    #            #if (self.grid.getVal(posx[1], posy[1]) >= 0.0):
+    #            #    self.grid.setVal(posx[1], posy[1], (self.grid.getVal(posx[1], posy[1]) + 2.0))
+    #            #mapIm[posy[1]][posx[1]] = 128
+    #            self.grid.setVal(posx[1], posy[1], (self.grid.getVal(posx[1], posy[1]) + 2.0))
+    #    if ((i != dest0) and (j != dest1)):
+    #        num_obstacles = 0
+    #        for t in range(i-1, i+2):
+    #            for p in range(j-1, j+2):
+    #                if ((t >= 0) and (t < 400) and (p >= 0) and (p < 400)):
+    #                    if (mapIm[p][t] == 0):
+    #                        num_obstacles = num_obstacles + 1
+    #        if (num_obstacles == 1):
+    #            #if (self.grid.getVal(i, j) < 0.0):
+    #            #    self.grid.setVal(i, j, 1000.0)
+    #            #else:
+    #            #if (self.grid.getVal(i, j) >= 0.0):
+    #            self.grid.setVal(i, j, self.grid.getVal(i, j)+10.0)
+    #            #mapIm[j][i] = 128
+    #            if ((i < dest0) and (j < dest1)):
+    #                posx = [i, i-1, i-1, i, i-1, i-2, i-2, i-2]
+    #                posy = [j-1, j, j-1, j-2, j-2, j-2, j-1, j]
+    #            if ((i < dest0) and (j > dest1)):
+    #                posx = [i, i-1, i-1, i, i-1, i-2, i-2, i-2]
+    #                posy = [j+1, j, j+1, j+2, j+2, j+2, j+1, j]
+    #            if ((i > dest0) and (j < dest1)):
+    #                posx = [i, i+1, i+1, i, i+1, i+2, i+2, i+2]
+    #                posy = [j-1, j, j-1, j-2, j-2, j-2, j-1, j]
+    #            if ((i > dest0) and (j > dest1)):
+    #                posx = [i, i+1, i+1, i, i+1, i+2, i+2, i+2]
+    #                posy = [j+1, j, j+1, j+2, j+2, j+2, j+1, j]
+    #            for u in range(0, len(posx)):
+    #                if (u < 3):
+    #                    if ((posx[u] >= 0) and (posx[u] < 400) and (posy[u] >= 0) and (posy[u] < 400) and (mapIm[posy[u]][posx[u]] == 255)):
+    #                        #if (self.grid.getVal(posx[u], posy[u]) < 0.0):
+    #                        #    self.grid.setVal(posx[u], posy[u], 1000.0)
+    #                        #else:
+    #                        #if (self.grid.getVal(posx[u], posy[u]) >= 0.0):
+    #                        #    self.grid.setVal(posx[u], posy[u], (self.grid.getVal(posx[u], posy[u]) + 5.0))
+    #                        #mapIm[posy[u]][posx[u]] = 128
+    #                        self.grid.setVal(posx[u], posy[u], (self.grid.getVal(posx[u], posy[u]) + 5.0))
+    #                else:
+    #                    if ((posx[u] >= 0) and (posx[u] < 400) and (posy[u] >= 0) and (posy[u] < 400) and (mapIm[posy[u]][posx[u]] == 255)):
+    #                        #if (self.grid.getVal(posx[u], posy[u]) < 0.0):
+    #                        #    self.grid.setVal(posx[u], posy[u], 1000.0)
+    #                        #else:
+    #                        #if (self.grid.getVal(posx[u], posy[u]) >= 0.0):
+    #                        #    self.grid.setVal(posx[u], posy[u], (self.grid.getVal(posx[u], posy[u]) + 2.0))
+    #                        #mapIm[posy[u]][posx[u]] = 128
+    #                        self.grid.setVal(posx[u], posy[u], (self.grid.getVal(posx[u], posy[u]) + 2.0)) 
   
 
     def generatePath(self):
@@ -138,6 +175,39 @@ class MyAlgorithm():
             self.grid.setVal(dest[0], dest[1], 20000.0)
 
         # Expansion of the field
+        #while (fin == "false"):
+        #    for i in range(dest[0]-square, dest[0]+square+1):
+        #        for j in range(dest[1]-square, dest[1]+square+1):
+        #            if ((i >= 0) and (i < 400) and (j >= 0) and (j < 400)):
+        #               for k in range(i-1, i+2):
+        #                   for l in range(j-1, j+2):
+        #                       if ((k >= 0) and (k < 400) and (l >= 0) and (l < 400)):
+        #                           val = self.grid.getVal(k, l)
+        #                           val_init = self.grid.getVal(i, j)
+        #                           if ((k == dest[0]) and (l == dest[1])):
+        #                               self.grid.setVal(k, l, self.grid.getVal(k, l))
+        #                           elif ((k == i) and (l == j)):
+        #                               self.grid.setVal(k, l, val_init+0.0)
+        #                           else:
+        #                               if ((k != i) and (l != j)):
+        #                                   if ((math.isnan(val)) or ((val_init + math.sqrt(2.0)) < val) or (val <= 0)):
+        #                                       self.grid.setVal(k, l, val_init+math.sqrt(2.0))
+        #                               else:
+        #                                   if ((math.isnan(val)) or ((val_init + 1.0) < val) or (val <= 0)):
+        #                                       self.grid.setVal(k, l, val_init+1.0)
+        #                               if (mapIm[l][k] == 0):
+        #                                   val_pos = self.grid.getVal(k, l)
+        #                                   self.grid.setVal(k, l, (val_pos+20000.0))
+        #                                   #if (self.grid.getVal(k, l) < 40000.0):
+        #                                   #    print "hi"
+        #                                   #    pos_obstacles_border.append([k, l])
+        #            # Cases of the margins
+        #            fin = self.findStopExpansion(dest, posRobot, margin, i, j, fin)
+        #    square = square + 1
+
+
+
+        # Expansion of the field
         while (fin == "false"):
             for i in range(dest[0]-square, dest[0]+square+1):
                 for j in range(dest[1]-square, dest[1]+square+1):
@@ -161,31 +231,60 @@ class MyAlgorithm():
                                        if (mapIm[l][k] == 0):
                                            val_pos = self.grid.getVal(k, l)
                                            self.grid.setVal(k, l, (val_pos+20000.0))
+                       if (((i < dest[0]-square+1) or (i > dest[0]+square-1)) or ((i >= dest[0]-square+1) and (i <= dest[0]+square-1) and ((j < dest[1]-square+1) or (j > dest[1]+square-1)))):
+                           if (mapIm[j][i] == 0):
+                               pos_obstacles_border.append([i,j])
                     # Cases of the margins
                     fin = self.findStopExpansion(dest, posRobot, margin, i, j, fin)
             square = square + 1
 
+        print len(pos_obstacles_border)
 
 
         # Obstacles penalties
-        fin_obstacles = "false"
-        squareO = 0
-        while (fin_obstacles == "false"):
-            for i in range(dest[0]-squareO, dest[0]+squareO+1):
-                for j in range(dest[1]-squareO, dest[1]+squareO+1):
-                    if ((i >= 0) and (i < 400) and (j >= 0) and (j < 400)):
-                        if (mapIm[j][i] == 0):
-                            #if (((i< dest[0]-squareO+1) or (i > dest[0]+squareO-1)) or ((i>= dest[0]-squareO+1) and (i<= dest[0]+squareO-1) and ((j< dest[1]-squareO+1) or (j> dest[1]+squareO-1)))):
-                            for k in range(i-3, i+4):
-                                for l in range(j-3, j+4):
-                                    if ((k >= 0) and (k < 400) and (l >= 0) and (l < 400)):
-                                        if (mapIm[l][k] == 255):
-                                            self.penaltiesObstacles(k, l, i, j)
-                                            print "k, l, i, j", k, l, i, j
-                                            #print "hi"
-                    # Cases of the margins
-                    fin_obstacles = self.findStopExpansion(dest, posRobot, margin, i, j, fin_obstacles)
-            squareO = squareO + 1
+        for i in range(0, len(pos_obstacles_border)):
+            if (mapIm[pos_obstacles_border[i][1]][pos_obstacles_border[i][0]] == 0):
+                for k in range(pos_obstacles_border[i][0]-3, pos_obstacles_border[i][0]+4):
+                    for l in range(pos_obstacles_border[i][1]-3, pos_obstacles_border[i][1]+4):
+                        if ((k >= 0) and (k < 400) and (l >= 0) and (l < 400)):
+                            if (mapIm[l][k] == 255):
+                                self.penaltiesObstacles(k, l, pos_obstacles_border[i][0], pos_obstacles_border[i][1])
+                                #print "k, l, i, j", k, l, pos_obstacles_border[i][0], pos_obstacles_border[i][1]
+
+        # MIRAR
+        # Obstacles penalties
+        #for i in range(0, 400):
+        #    for j in range(0, 400):
+        #        #if ((i >= 0) and (i < 400) and (j >= 0) and (j < 400)):
+        #        if (mapIm[j][i] == 0):
+        #            for k in range(i-3, i+4):
+        #                for l in range(j-3, j+4):
+        #                    if ((k >= 0) and (k < 400) and (l >= 0) and (l < 400)):
+        #                        if (mapIm[l][k] == 255):
+        #                            self.penaltiesObstacles(k, l, i, j)
+        #                            print "k, l, i, j", k, l, i, j#
+
+
+
+        # Obstacles penalties
+        #fin_obstacles = "false"
+        #squareO = 0
+        #while (fin_obstacles == "false"):
+        #    for i in range(dest[0]-squareO, dest[0]+squareO+1):
+        #        for j in range(dest[1]-squareO, dest[1]+squareO+1):
+        #            if ((i >= 0) and (i < 400) and (j >= 0) and (j < 400)):
+        #                if (mapIm[j][i] == 0):
+        #                    #if (((i< dest[0]-squareO+1) or (i > dest[0]+squareO-1)) or ((i>= dest[0]-squareO+1) and (i<= dest[0]+squareO-1) and ((j< dest[1]-squareO+1) or (j> dest[1]+squareO-1)))):
+        #                    for k in range(i-3, i+4):
+        #                        for l in range(j-3, j+4):
+        #                            if ((k >= 0) and (k < 400) and (l >= 0) and (l < 400)):
+        #                                if (mapIm[l][k] == 255):
+        #                                    self.penaltiesObstacles(k, l, i, j)
+        #                                    print "k, l, i, j", k, l, i, j
+        #                                    #print "hi"
+        #            # Cases of the margins
+        #            fin_obstacles = self.findStopExpansion(dest, posRobot, margin, i, j, fin_obstacles)
+        #    squareO = squareO + 1
 
 
         # Find the path
