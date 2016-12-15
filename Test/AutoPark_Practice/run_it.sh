7eaec40d73dfb3224c3901eeb57b49fb1609884a
vmartinezf <vanessa_1895@msn.com>
2016-12-15 12:58:10 +0100

    autoparking

---
+++ Test/AutoPark_Practice/run_it.sh
@@ -0,0 +1,19 @@
+
+#!/bin/sh
+#
+# Copyright (c) 2016
+# Author: Victor Arribas <v.arribas.urjc@gmail.com>
+# License: GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
+
+world=autopark.world
+#world=circuit.world
+
+gzserver --verbose --minimal_comms $world &
+sleep 5 # up to 20 for circuit.world
+
+[ "$1" = "GUI" ] && gzclient &
+
+python3 obstacle_avoidance.py --Ice.Config=obstacle_avoidance.cfg
+
+killall gzserver
+[ "$1" = "GUI" ] && killall gzclient
