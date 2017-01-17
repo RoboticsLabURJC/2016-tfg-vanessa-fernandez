1a7e782ff6a23a7cd7827e94710b055af8d92716
vmartinezf <vanessa_1895@msn.com>
2017-01-17 10:47:45 +0100

    creando matrices RT

--- Test/AutoPark_Practice/gui/widgets/mapWidget.py
+++ Test/AutoPark_Practice/gui/widgets/mapWidget.py
@@ -21,7 +21,7 @@
 from PyQt5.QtWidgets import QWidget, QGridLayout
 from PyQt5.QtGui import QPen, QPainter
 from PyQt5.QtCore import QPoint, QPointF, pyqtSignal, Qt
-from numpy import matrix
+import numpy as np
 import math
 
 class MapWidget(QWidget):
@@ -71,7 +71,7 @@ class MapWidget(QWidget):
         colorLaser1 = Qt.blue
         colorLaser2 = Qt.green
         colorLaser3 = Qt.red
-        self.drawLaser1(painter, colorLaser1)
+        self.drawLaser1(painter, colorLaser1,-50)
         self.drawLaser2(painter, colorLaser2)
         self.drawLaser3(painter, colorLaser3)
 
@@ -98,16 +98,21 @@ class MapWidget(QWidget):
         painter.fillRect(-carsize/2,carsize-2*carsize/5,carsize/5,2*carsize/5,Qt.black)
         painter.fillRect(carsize/2,carsize-2*carsize/5,-carsize/5,2*carsize/5,Qt.black)
 
-    def drawLaser1(self, painter, color):
+
+    def RT1(self, d0, d1):
+        RT = np.array([[math.cos(d1), 0, 0], [math.sin(d1), 0, 0], [0, 0, 1]])
+        return RT
+
+    def drawLaser1(self, painter, color, traslate):
         pen = QPen(color, 2)
         painter.setPen(pen)
         for d in self.laser1:
             #px = d[0]*math.cos(d[1])*self.scale
             #py = -d[0]*math.sin(d[1])*self.scale
-            #[[px], [py], [1]] = np.empty((2, 3))
-            #[[px], [py], [1]] = [[math.cos(d[1]), 0, 0], [math.sin(d[1]), 0, 0], [0, 0, 1]]*[[d[0]], [-d[0]], [1]] *self.scale
-            poses = matrix([[math.cos(d[1]), 0, 0], [math.sin(d[1]), 0, 0], [0, 0, 1]])*matrix([[d[0]], [-d[0]], [1]]) *self.scale
-            painter.drawLine(QPointF(0,-50),QPointF(px, py-50))
+            #poses = np.array([[math.cos(d[1]), 0, 0], [math.sin(d[1]), 0, 0], [0, 0, 1]])*np.array([[d[0]], [-d[0]], [1]]) *self.scale
+            RT = self.RT1(d[0], d[1])
+            poses = RT*np.array([[d[0]], [-d[0]], [1]]) *self.scale
+            painter.drawLine(QPointF(0,traslate),QPointF(poses[0][0], poses[1][0]+traslate))
 
 
     def drawLaser2(self, painter, color):
