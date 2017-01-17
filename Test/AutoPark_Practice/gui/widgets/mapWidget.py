64cd29efa3031605c7c91dbc210cd6e93477938e
vmartinezf <vanessa_1895@msn.com>
2017-01-17 17:35:19 +0100

    cambios en RT

--- Test/AutoPark_Practice/gui/widgets/mapWidget.py
+++ Test/AutoPark_Practice/gui/widgets/mapWidget.py
@@ -71,9 +71,9 @@ class MapWidget(QWidget):
         colorLaser1 = Qt.blue
         colorLaser2 = Qt.green
         colorLaser3 = Qt.red
-        self.drawLaser1(painter, colorLaser1,-50)
-        self.drawLaser2(painter, colorLaser2)
-        self.drawLaser3(painter, colorLaser3)
+        self.drawLaser(1, painter, colorLaser1, 0, -50, self.laser1)
+        self.drawLaser(2, painter, colorLaser2, 0, 50, self.laser2)
+        self.drawLaser(3, painter, colorLaser3, -25, 0, self.laser3)
 
 
         # Draw car
@@ -86,9 +86,6 @@ class MapWidget(QWidget):
         pen = QPen(Qt.black, 1)
         painter.setPen(pen)
 
-        # Connectors
-        #painter.drawLine(QPointF(-carsize/5,carsize/5),QPointF(carsize/5, carsize/5))
-
         # Chassis
         painter.fillRect(-carsize/2, -carsize,carsize,2*carsize,Qt.yellow)
 
@@ -99,39 +96,36 @@ class MapWidget(QWidget):
         painter.fillRect(carsize/2,carsize-2*carsize/5,-carsize/5,2*carsize/5,Qt.black)
 
 
-    def RT1(self, d0, d1):
-        RT = np.array([[math.cos(d1), 0, 0], [math.sin(d1), 0, 0], [0, 0, 1]])
+    def RTLaser1(self, angle):
+        RT = np.array([[math.cos(angle), 0, 0], [-math.sin(angle), 0, 0], [0, 0, 1]])
         return RT
 
-    def drawLaser1(self, painter, color, traslate):
-        pen = QPen(color, 2)
-        painter.setPen(pen)
-        for d in self.laser1:
-            #px = d[0]*math.cos(d[1])*self.scale
-            #py = -d[0]*math.sin(d[1])*self.scale
-            #poses = np.array([[math.cos(d[1]), 0, 0], [math.sin(d[1]), 0, 0], [0, 0, 1]])*np.array([[d[0]], [-d[0]], [1]]) *self.scale
-            RT = self.RT1(d[0], d[1])
-            poses = RT*np.array([[d[0]], [-d[0]], [1]]) *self.scale
-            painter.drawLine(QPointF(0,traslate),QPointF(poses[0][0], poses[1][0]+traslate))
+    def RTLaser2(self, angle):
+        RT = np.array([[-math.cos(angle), 0, 0], [math.sin(angle), 0, 0], [0, 0, 1]])
+        return RT
+
+    def RTLaser3(self, angle):
+        RT = np.array([[-math.sin(angle), 0, 0], [-math.cos(angle), 0, 0], [0, 0, 1]])
+        return RT
 
 
-    def drawLaser2(self, painter, color):
-        pen = QPen(color, 2)
-        painter.setPen(pen)
-        for d in self.laser2:
-            px = -d[0]*math.cos(d[1])*self.scale
-            py = d[0]*math.sin(d[1])*self.scale
-            painter.drawLine(QPointF(0,50),QPointF(px, py+50))
-            
 
-    def drawLaser3(self, painter, color):
+    def drawLaser(self, numLaser, painter, color, xTraslate, yTraslate, laser):
         pen = QPen(color, 2)
         painter.setPen(pen)
-        for d in self.laser3:
-            px = -d[0]*math.sin(d[1])*self.scale
-            py = -d[0]*math.cos(d[1])*self.scale
-            painter.drawLine(QPointF(-25,0),QPointF(px-25, py))
-            
+        for d in laser:
+            dist = d[0]
+            angle = d[1]
+            if (numLaser == 1):
+                RT = self.RTLaser1(angle)
+            elif (numLaser == 2):
+                RT = self.RTLaser2(angle)
+            else:
+                RT = self.RTLaser3(angle)
+            orig_poses = np.array([[dist], [dist], [1]]) * self.scale
+            final_poses = RT * orig_poses
+            painter.drawLine(QPointF(xTraslate,yTraslate),QPointF(final_poses[0][0] + xTraslate, final_poses[1][0]+yTraslate))
+
 
     def setCarArrow(self, x, y):
         self.carx = x
