0f9862fa400e3afb33af0c0ceaaf1e2bb7bcd45a
vmartinezf <vanessa_1895@msn.com>
2017-02-08 13:25:33 +0100

    GUI cambios

--- Test/AutoPark_Practice/gui/GUI.py
+++ Test/AutoPark_Practice/gui/GUI.py
@@ -19,6 +19,8 @@
 
 from gui.widgets.teleopWidget import TeleopWidget
 from gui.widgets.mapWidget import MapWidget
+from gui.widgets.mapWidget import MapWidget1
+#from gui.widgets.mapWidget1 import MapWidget1
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import QMainWindow
 from gui.form import Ui_MainWindow
@@ -32,31 +34,41 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.teleop=TeleopWidget(self)
         self.map=MapWidget(self)
+        self.map1=MapWidget1(self)
         self.tlLayout.addWidget(self.teleop)
         self.teleop.setVisible(True)
         self.mapLayout.addWidget(self.map)
+        self.map1Layout.addWidget(self.map1)
         self.map.setVisible(True)
+        self.map1.setVisible(True)
 
         self.pushButton.clicked.connect(self.playClicked)
         self.pushButton.setCheckable(True)
         self.updGUI.connect(self.updateGUI)
+        #self.camera1=CameraWidget(self)
 
         self.stopButton.clicked.connect(self.stopClicked)
 
     def updateGUI(self):
+        #self.camera1.updateImage()
         (cx, cy) = self.algorithm.getCarDirection()
-        (ox, oy) = self.algorithm.getObstaclesDirection()
-        (ax, ay) = self.algorithm.getAverageDirection()
+        #(ox, oy) = self.algorithm.getObstaclesDirection()
+        #(ax, ay) = self.algorithm.getAverageDirection()
         laserdata1 = self.laser1.getLaserData()
         laserdata2 = self.laser2.getLaserData()
         laserdata3 = self.laser3.getLaserData()
         if (laserdata1):
             self.map.setLaserValues(1,laserdata1)
+            self.map1.setLaserValues(1,laserdata1)
         if (laserdata2):
             self.map.setLaserValues(2,laserdata2)
+            self.map1.setLaserValues(2,laserdata2)
         if (laserdata3):
             self.map.setLaserValues(3,laserdata3)
+            self.map1.setLaserValues(3,laserdata3)
         self.map.update()
+        self.map1.update()
+
 
     def getPose3D(self):
         return self.pose3d
