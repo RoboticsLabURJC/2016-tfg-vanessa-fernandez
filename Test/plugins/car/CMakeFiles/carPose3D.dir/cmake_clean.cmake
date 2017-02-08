f40b0aff4f0765040bc116574c7528fc4491262c
vmartinezf <vanessa_1895@msn.com>
2017-02-08 11:08:16 +0100

    cambios en el plugin car

---
+++ Test/plugins/car/CMakeFiles/carPose3D.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/carPose3D.dir/pose3d.cc.o"
+  "libcarPose3D.pdb"
+  "libcarPose3D.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/carPose3D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
