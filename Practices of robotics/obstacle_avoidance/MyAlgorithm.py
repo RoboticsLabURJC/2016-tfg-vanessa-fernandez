1a3d0daf4aae368bf766013cec89cea1b9125d7a
vmartinezf <vanessa_1895@msn.com>
2017-02-08 17:34:26 +0100

    nueva version obstacle avoidance

---
+++ Practices of robotics/obstacle_avoidance/MyAlgorithm.py
@@ -0,0 +1,247 @@
+import numpy as np
+import threading
+import time
+from datetime import datetime
+import jderobot
+import math
+from Target import Target
+from Parser import Parser
+
+time_cycle = 80
+
+
+def absolutas2relativas(x, y, rx, ry, rt):
+    # Convert to relatives
+    dx = x - rx
+    dy = y - ry
+
+    # Rotate with current angle
+    x = dx*math.cos(-rt) - dy*math.sin(-rt)
+    y = dx*math.sin(-rt) + dy*math.cos(-rt)
+
+    return x,y
+
+
+def parse_laser_data(laser_data):
+    laser = []
+    for i in range(laser_data.numLaser):
+        dist = laser_data.distanceData[i]/1000.0
+        angle = math.radians(i)
+        laser += [(dist, angle)]
+    return laser
+
+
+
+class MyAlgorithm(threading.Thread):
+
+    def __init__(self, cameraL, cameraR, pose3d, laser, motors):
+        self.cameraL = cameraL
+        self.cameraR = cameraR
+        self.pose3d = pose3d
+        self.laser = laser
+        self.motors = motors
+
+        self.imageRight=None
+        self.imageLeft=None
+
+        # Car direction
+        self.carx = 0.0
+        self.cary = 0.0
+
+        # Obstacles direction
+        self.obsx = 0.0
+        self.obsy = 0.0
+
+        # Average direction
+        self.avgx = 0.0
+        self.avgy = 0.0
+
+        # Current target
+        self.targetx = 0.0
+        self.targety = 0.0
+
+        self.stop_event = threading.Event()
+        self.kill_event = threading.Event()
+        self.lock = threading.Lock()
+        threading.Thread.__init__(self, args=self.stop_event)
+
+        # Init targets
+        parser = Parser('targets.json')
+        self.targets = parser.getTargets()
+
+    def getNextTarget(self):
+        for target in self.targets:
+            if target.isReached() == False:
+                return target
+
+        return None
+
+    def setRightImageFiltered(self, image):
+        self.lock.acquire()
+        self.imageRight=image
+        self.lock.release()
+
+
+    def setLeftImageFiltered(self, image):
+        self.lock.acquire()
+        self.imageLeft=image
+        self.lock.release()
+
+    def getRightImageFiltered(self):
+        self.lock.acquire()
+        tempImage=self.imageRight
+        self.lock.release()
+        return tempImage
+
+    def getLeftImageFiltered(self):
+        self.lock.acquire()
+        tempImage=self.imageLeft
+        self.lock.release()
+        return tempImage
+
+    def getCarDirection(self):
+        return (self.carx, self.cary)
+
+    def getObstaclesDirection(self):
+        return (self.obsx, self.obsy)
+
+    def getAverageDirection(self):
+        return (self.avgx, self.avgy)
+
+    def getCurrentTarget(self):
+        return (self.targetx, self.targety)
+
+    def run (self):
+
+        while (not self.kill_event.is_set()):
+           
+            start_time = datetime.now()
+
+            if not self.stop_event.is_set():
+                self.execute()
+
+            finish_Time = datetime.now()
+
+            dt = finish_Time - start_time
+            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
+            #print (ms)
+            if (ms < time_cycle):
+                time.sleep((time_cycle - ms) / 1000.0)
+
+    def stop (self):
+        self.stop_event.set()
+
+    def play (self):
+        if self.is_alive():
+            self.stop_event.clear()
+        else:
+            self.start()
+
+    def kill (self):
+        self.kill_event.set()
+
+
+    def execute(self):
+        self.currentTarget = self.getNextTarget()
+        self.targetx = self.currentTarget.getPose().x
+        self.targety = self.currentTarget.getPose().y
+
+        # TODO
+        # Get the position of the robot
+        rx = self.pose3d.getX()/1000
+        ry = self.pose3d.getY()/1000
+
+        # We get the orientation of the robot with respect to the map
+        rt = self.pose3d.getYaw()
+
+
+        # We marked the subgoals as we go through
+        if(abs(ry)<(abs(self.targety)+1) and abs(ry)>(abs(self.targety)-1)):
+            self.currentTarget.setReached(True)
+
+
+        # Get the data of the laser sensor, which consists of 180 pairs of values
+        laser_data = self.laser.getLaserData()
+        laser = parse_laser_data(laser_data)
+        # Convert self.targetx y self.targety to relative coordinates
+        self.carx,self.cary = absolutas2relativas(self.targetx,self.targety,rx,ry,rt)
+
+
+        # Laser       
+        laser_vectorized = []
+        for d,a in laser:
+            # (4.2.1) laser into GUI reference system
+            x = d * math.cos(a) * -1
+            y = d * math.sin(a) * -1
+            v = (x,y)
+            laser_vectorized += [v]
+
+        # Average of the 180 values of the laser
+        laser_mean = np.mean(laser_vectorized, axis=0)
+
+
+        # Repulsor vector
+        dist_threshold = 6
+        vff_repulsor_list = []
+        for d,a in laser:
+            # (4.2.1) laser into GUI reference system
+            if(d < dist_threshold):
+                x = (d - dist_threshold) * math.cos(a) * -1
+                y = (d - dist_threshold) * math.sin(a) * -1
+                v = (x,y)
+                vff_repulsor_list += [v]
+
+        vff_repulsor = np.mean(vff_repulsor_list, axis=0)
+
+        self.obsx,self.obsy = vff_repulsor
+
+        # Calculating repulsor vector module
+        mod_repulsor = pow(pow(self.obsx,2) + pow(self.obsy,2),0.5)
+        if (mod_repulsor > 1.55):
+            self.obsx,self.obsy = vff_repulsor * 4.5
+
+
+        # Calculating the coordinates of the resultant vector
+        self.avgx = self.carx + self.obsx
+        self.avgy = self.cary + self.obsy
+
+
+        # Calculating the module of the speed
+        speed = pow(pow(self.avgx,2) + pow(self.avgy,2),0.5)
+
+        # Correction
+        if (abs(self.obsx) > 2):
+            if (abs(self.obsx) < abs(self.carx)):
+                if (self.obsx >= 0):
+                    self.avgx = abs(self.avgx)
+                else:
+                    self.avgx = -abs(self.avgx)
+
+
+        if ((self.obsx == (-self.carx)) and (self.obsy == (-self.cary))):
+            self.avgx = self.obsx
+            self.avgy = self.cary
+
+
+        # Calculating angle
+        if (speed < 1):
+            # Use the tangent to avoid indeterminacy
+            angle = math.atan(abs(self.avgx/self.avgy))
+        else:
+            angle = math.asin(abs(self.avgx/speed))
+        if(self.avgy > 0):
+            angle = 0.3*(math.pi - angle)
+
+
+        # Linear speed
+        if ((speed < 1) or (speed > 3)):
+            self.motors.sendV(3)
+        else:
+            self.motors.sendV(speed)
+
+        # Angular speed
+        if(self.avgx < 0):
+            self.motors.sendW(-angle * 0.75)
+        else:
+            self.motors.sendW(angle * 0.75)
+
---
+++ Practices of robotics/obstacle_avoidance/gui/widgets/mapWidget.py
@@ -0,0 +1,198 @@
+#
+#  Copyright (C) 1997-2016 JDE Developers Team
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see http://www.gnu.org/licenses/.
+#  Authors :
+#       Eduardo Perdices <eperdices@gsyc.es>
+#
+
+#import resources_rc
+from PyQt5.QtWidgets import QWidget, QGridLayout
+from PyQt5.QtGui import QPen, QPainter
+from PyQt5.QtCore import QPoint, QPointF, pyqtSignal, Qt
+import math
+
+class MapWidget(QWidget):
+
+    stopSIG=pyqtSignal()
+    
+    def __init__(self,winParent):    
+        super(MapWidget, self).__init__()
+        self.winParent=winParent
+        self.initUI()
+
+        self.carx = 0.0
+        self.cary = 0.0
+        self.obsx = 0.0
+        self.obsy = 0.0
+        self.avgx = 0.0
+        self.avgy = 0.0
+        self.targetx = 0.0
+        self.targety = 0.0
+        self.scale = 30.0
+        self.laser = []
+        
+    def initUI(self):
+        layout=QGridLayout()  
+        self.setLayout(layout)
+        self.setAutoFillBackground(True)
+        p = self.palette()
+        p.setColor(self.backgroundRole(), Qt.white)
+        self.setPalette(p)
+        self.resize(300,300)
+        self.setMinimumSize(300,300)
+        
+    def paintEvent(self, e):
+        _width = self.width()
+        _height = self.height()
+    
+        painter=QPainter(self)
+        pen = QPen(Qt.blue, 2)
+        painter.setPen(pen)
+    
+        #Widget center
+        painter.translate(QPoint(_width/2, _height/1.2))
+           
+        # Draw car
+        self.drawCar(painter)
+
+        # Draw laser
+        self.drawLasel(painter)
+
+        # Draw target
+        self.drawTarget(painter, self.targetx, self.targety)
+
+        # Draw arrows
+        self.drawArrow(painter, self.carx, self.cary, Qt.green, 2)
+        self.drawArrow(painter, self.obsx, self.obsy, Qt.red, 2)
+        self.drawArrow(painter, self.avgx, self.avgy, Qt.black, 2)
+
+    def drawCar(self, painter):
+        carsize = 30
+        tiresize = carsize/5
+
+        pen = QPen(Qt.black, 1)
+        painter.setPen(pen)
+
+        # Connectors
+        painter.drawLine(QPointF(-carsize/5,carsize/5),QPointF(carsize/5, carsize/5))
+
+        # Chassis
+        painter.fillRect(-carsize/6,carsize/2,carsize/3,carsize/2,Qt.red)
+        painter.fillRect(-carsize/16,0,carsize/8,carsize,Qt.red)
+        painter.fillRect(-carsize/6,-carsize/24,carsize/3,carsize/12,Qt.red)
+        painter.fillRect(-carsize/8,carsize-carsize/96,carsize/4,carsize/12,Qt.red)
+
+        # Tires
+        painter.fillRect(-carsize/4,carsize/8,tiresize/2,tiresize,Qt.black)
+        painter.fillRect(carsize/4,carsize/8,-tiresize/2,tiresize,Qt.black)
+        painter.fillRect(-carsize/4,carsize-carsize/8,tiresize/2,tiresize,Qt.black)
+        painter.fillRect(carsize/4,carsize-carsize/8,-tiresize/2,tiresize,Qt.black)
+
+    def drawLasel(self, painter):
+        pen = QPen(Qt.blue, 2)
+        painter.setPen(pen)
+        for d in self.laser:
+            px = -d[0]*math.sin(d[1])*self.scale
+            py = d[0]*math.cos(d[1])*self.scale
+            painter.drawLine(QPointF(0,0),QPointF(py, px))
+            
+    def drawArrow(self, painter, posx, posy, color, width):
+        if posx == 0.0 and posy == 0.0:
+            return        
+
+        _width = self.width()
+        _height = self.height()
+
+        pen = QPen(color, width)
+        painter.setPen(pen)
+
+        # Draw main line
+        px = _width/2*posx/10.0
+        py = _height/2*posy/10.0
+        painter.drawLine(QPointF(0,0),QPointF(-px, py))
+
+        # Draw sides
+        sidex = math.hypot(px, py)/5.0
+        sidey = math.hypot(px, py)/5.0
+        if px != 0.0:
+            ang = math.atan(py/px)
+        else:
+            ang = math.pi/2.0
+        if posx >= 0.0:
+            px1 = px + sidex * math.cos(math.pi+ang-0.5)
+            py1 = py + sidey * math.sin(math.pi+ang-0.5)
+            px2 = px + sidex * math.cos(math.pi+ang+0.5)
+            py2 = py + sidey * math.sin(math.pi+ang+0.5)
+        else:
+            px1 = px - sidex * math.cos(math.pi+ang-0.5)
+            py1 = py - sidey * math.sin(math.pi+ang-0.5)
+            px2 = px - sidex * math.cos(math.pi+ang+0.5)
+            py2 = py - sidey * math.sin(math.pi+ang+0.5)    
+        painter.drawLine(QPointF(-px, py),QPointF(-px1, py1))
+        painter.drawLine(QPointF(-px, py),QPointF(-px2, py2))
+
+    def drawTarget(self, painter, posx, posy):
+        pen = QPen(Qt.red, 4)
+        painter.setPen(pen)
+
+        sx = posx - 0.25
+        sy = posy - 0.25
+        ex = posx + 0.25
+        ey = posy + 0.25
+        painter.drawLine(QPointF(-sx*self.scale,sy*self.scale),QPointF(-ex*self.scale,ey*self.scale))
+
+        sx = posx + 0.25
+        sy = posy - 0.25
+        ex = posx - 0.25
+        ey = posy + 0.25
+        painter.drawLine(QPointF(-sx*self.scale,sy*self.scale),QPointF(-ex*self.scale,ey*self.scale))
+
+    def setCarArrow(self, x, y):
+        self.carx = x
+        self.cary = y
+
+    def setObstaclesArrow(self, x, y):
+        self.obsx = x
+        self.obsy = y
+
+    def setAverageArrow(self, x, y):
+        self.avgx = x
+        self.avgy = y
+
+    def setTarget(self, x, y, rx, ry, rt):
+        # Convert to relatives
+        dx = x - rx
+        dy = y - ry
+
+        # Rotate with current angle
+        self.targetx = dx*math.cos(-rt) - dy*math.sin(-rt)
+        self.targety = dx*math.sin(-rt) + dy*math.cos(-rt)
+
+    def setLaserValues(self, laser):
+        # Init laser array
+        if len(self.laser) == 0:
+            for i in range(laser.numLaser):
+                self.laser.append((0,0))
+
+        for i in range(laser.numLaser):
+            dist = laser.distanceData[i]/1000.0
+            angle = math.radians(i)
+            self.laser[i] = (dist, angle)
+
+
+
+ 
+       
+
