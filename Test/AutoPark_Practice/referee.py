378f1b26f5238d0b9debd4caef9077fd7aae5b94
vmartinezf <vanessa_1895@msn.com>
2017-03-24 13:46:48 +0100

    testDistance

--- Test/AutoPark_Practice/referee.py
+++ Test/AutoPark_Practice/referee.py
@@ -291,7 +291,33 @@ class notaWidget(QWidget):
         return notaTime
     
     def testDistance(self):
-        notaDist = 0
+        distancia = distanciaWidget(self,pose3d)
+        MyDistFront = distancia.distFrontFinal
+        MyDistRear = distancia.distRearFinal
+        MyDistSidewalk = distancia.distanceSidewalk
+
+        if MyDistFront >= 2 and MyDistFront < 3.5:
+            notaDistFront = 100
+        elif MyDistFront < 2 and MyDistFront >= 1:
+            notaDistFront = 50
+        else:
+            notaDistFront = 0
+
+        if MyDistRear >= 2 and MyDistRear < 3.5:
+            notaDistRear = 100
+        elif MyDistRear < 2 and MyDistRear >= 1:
+            notaDistRear = 50
+        else:
+            notaDistRear = 0
+
+        if MyDistSidewalk <= 0.75:
+            notaDistSidewalk = 100
+        elif MyDistSidewalk > 0.75 and MyDistSidewalk < 1.5:
+            notaDistSidewalk = 50
+        else:
+            notaDistSidewalk = 0
+
+        notaDist = notaDistFront*1/3 + notaDistRear*1/3 + notaDistSidewalk*1/3
         return notaDist
     
     def testCollision(self):
