d20f633b203a64dd4eaaa7174be646483200c9c3
vmartinezf <vanessa_1895@msn.com>
2017-03-29 21:40:06 +0200

    creando archivos

---
+++ Test/Stop_Practice/MyAlgorithm.py
@@ -0,0 +1,61 @@
+import numpy as np
+import threading
+import time
+from datetime import datetime
+import jderobot
+import math
+import cv2
+
+time_cycle = 80
+
+
+class MyAlgorithm(threading.Thread):
+
+    def __init__(self, cameraL, cameraR, pose3d, motors):
+        self.cameraL = cameraL
+        self.cameraR = cameraR
+        self.pose3d = pose3d
+        self.motors = motors
+
+        self.imageRight=None
+        self.imageLeft=None
+
+        self.stop_event = threading.Event()
+        self.kill_event = threading.Event()
+        self.lock = threading.Lock()
+        threading.Thread.__init__(self, args=self.stop_event)
+
+
+    def run (self):
+
+        while (not self.kill_event.is_set()):
+           
+            start_time = datetime.now()
+
+            if not self.stop_event.is_set():
+                self.execute()
+
+            finish_Time = datetime.now()
+
+            dt = finish_Time - start_time
+            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
+            #print (ms)
+            if (ms < time_cycle):
+                time.sleep((time_cycle - ms) / 1000.0)
+
+    def stop (self):
+        self.stop_event.set()
+
+    def play (self):
+        if self.is_alive():
+            self.stop_event.clear()
+        else:
+            self.start()
+
+    def kill (self):
+        self.kill_event.set()
+
+
+    def execute(self):
+
+        # TODO    
