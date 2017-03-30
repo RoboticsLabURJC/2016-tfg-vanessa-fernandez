18b9972ac3aa45e9b183ff6cbfe43caab6d0d61e
vmartinezf <vanessa_1895@msn.com>
2017-03-30 16:15:35 +0200

    stop

--- Test/Stop_Practice/gui/GUI.py
+++ Test/Stop_Practice/gui/GUI.py
@@ -19,7 +19,7 @@
 
 from gui.widgets.teleopWidget import TeleopWidget
 from gui.widgets.mapWidget import MapWidget
-from gui.widgets.mapWidget import MapWidget1
+#from gui.widgets.mapWidget import MapWidget1
 from gui.widgets.mapWidget import LogoWidget
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import QMainWindow
@@ -33,28 +33,26 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
         self.teleop=TeleopWidget(self)
-        self.map=MapWidget(self)
-        self.map1=MapWidget1(self)
+        #self.map=MapWidget(self)
         self.logo = LogoWidget(self)
         self.tlLayout.addWidget(self.teleop)
         self.teleop.setVisible(True)
-        self.mapLayout.addWidget(self.map)
-        self.map1Layout.addWidget(self.map1)
+        #self.mapLayout.addWidget(self.map)
         self.logoLayout.addWidget(self.logo)
-        self.map.setVisible(True)
-        self.map1.setVisible(True)
+        #self.map.setVisible(True)
         self.logo.setVisible(True)
 
         self.pushButton.clicked.connect(self.playClicked)
         self.pushButton.setCheckable(True)
         self.updGUI.connect(self.updateGUI)
+        self.camera1=CameraWidget(self)
 
         self.stopButton.clicked.connect(self.stopClicked)
 
     def updateGUI(self):
         self.camera1.updateImage()
-        self.map.update()
-        self.map1.update()
+       # self.map.update()
+        #self.map1.update()
 
     def getPose3D(self):
         return self.pose3d
