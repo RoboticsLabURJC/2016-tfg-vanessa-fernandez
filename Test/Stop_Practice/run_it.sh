4e687cf9258a0adb576ba2ad300ae9f23d2d725c
vmartinezf <vanessa_1895@msn.com>
2017-04-26 12:43:05 +0200

    python2

--- Test/Stop_Practice/run_it.sh
+++ Test/Stop_Practice/run_it.sh
@@ -6,15 +6,14 @@
 # License: GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
 
 world=stop.world
-#world=circuit.world
 
 gzserver --verbose --minimal_comms $world &
-sleep 5 # up to 20 for circuit.world
+sleep 5 # up to 20 for stop.world
 
 [ "$1" = "GUI" ] && gzclient &
 
-python3 stop.py --Ice.Config=stop.cfg
+python2 stop.py --Ice.Config=stop.cfg
 
 killall gzserver
-killall python3
+killall python2
 [ "$1" = "GUI" ] && killall gzclient
