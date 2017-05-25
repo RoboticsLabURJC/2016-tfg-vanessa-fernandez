fc7506d4b8d3aa488234e1a8d7fbdb4d73ec57fa
vmartinezf <vanessa_1895@msn.com>
2017-05-25 13:39:42 +0200

    choques

--- Test/Vacuum_Practice/MyAlgorithm.py
+++ Test/Vacuum_Practice/MyAlgorithm.py
@@ -5,19 +5,24 @@ from datetime import datetime
 import jderobot
 import math
 import cv2
+from math import pi as pi
 
 time_cycle = 80
         
 
 class MyAlgorithm(threading.Thread):
 
-    def __init__(self, pose3d, motors, laser):
+    def __init__(self, pose3d, motors, laser, bumper):
         self.pose3d = pose3d
         self.motors = motors
         self.laser = laser
+        self.bumper = bumper
         self.grid = np.empty([300,300],float)
         self.radiusInitial = 0.1
         self.constant = 0.01
+        self.numCrash = 0
+        self.angle = 0
+        self.yaw = 0
 
 
         self.stop_event = threading.Event()
@@ -75,11 +80,32 @@ class MyAlgorithm(threading.Thread):
 
         print ('Execute')
         # TODO
-        self.motors.sendW(0.5)
-        self.motors.sendV(self.radiusInitial*self.constant)
-        self.constant += 0.012
-        #print(self.constant)
-        #print(self.constant*self.radiusInitial)
+
+        # Devuelve 1 si choca y 0 si no choca
+        crash = self.bumper.getBumperData().state
+
+        if crash == 1:
+            self.numCrash = 1
+
+        print(self.numCrash)
+        self.yaw = self.pose3d.getYaw()
+
+        if self.numCrash == 0:
+            self.motors.sendW(0.5)
+            self.motors.sendV(self.radiusInitial*self.constant)
+            self.constant += 0.012
+        else:
+            while self.angle == 0:
+                angle = abs(self.yaw - self.pose3d.getYaw())
+                if angle != pi/6:
+                    self.motors.sendW(pi/6)
+                    self.motors.sendV(0)
+                    print("entrando")
+                else:
+                    self.angle = pi/6
+            self.motors.sendV(2)
+            
+
         # www.sr.echu.es/sbweb/fisica/celeste/espiral/espiral.html
         # proyectodescartes.org/descartescms/blog/itemlist/tag/espirales
         # http://www2.famaf.unc.edu.ar/rev_edu/documents/vol_23/prop_11_La_espiral_de_Arquimedes.pdf
