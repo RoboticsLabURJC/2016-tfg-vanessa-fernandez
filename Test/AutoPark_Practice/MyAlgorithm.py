7d473285f1e2764e8fa03e7418fc3f5e498fe0c5
vmartinezf <vanessa_1895@msn.com>
2017-06-05 12:07:37 +0200

    eliminando cosas

--- Test/AutoPark_Practice/MyAlgorithm.py
+++ Test/AutoPark_Practice/MyAlgorithm.py
@@ -5,52 +5,30 @@ from datetime import datetime
 import jderobot
 import math
 import cv2
+from ompl.util import OutputHandler, useOutputHandler, LogLevel, OMPL_DEBUG, OMPL_INFORM, OMPL_WARN, OMPL_ERROR
+from ompl import base as ob
+from ompl import geometric as og
+from ompl import control as oc
+from ompl import app as oa
+
+import ompl
 
 time_cycle = 80
 
 
 class MyAlgorithm(threading.Thread):
 
-    #def __init__(self, cameraL, cameraR, pose3d, laser, motors):
     def __init__(self, pose3d, laser1, laser2, laser3, motors):
-        #self.cameraL = cameraL
-        #self.cameraR = cameraR
         self.pose3d = pose3d
         self.laser1 = laser1
         self.laser2 = laser2
         self.laser3 = laser3
         self.motors = motors
-        #self.grid = np.empty([300, 300], float)
-
-        #self.imageRight=None
-        #self.imageLeft=None
-
-        # Car direction
-        self.carx = 0.0
-        self.cary = 0.0
-
-        # Obstacles direction
-      #  self.obsx = 0.0
-      #  self.obsy = 0.0
-
-        # Average direction
-      #  self.avgx = 0.0
-      #  self.avgy = 0.0
 
         self.stop_event = threading.Event()
         self.kill_event = threading.Event()
         self.lock = threading.Lock()
         threading.Thread.__init__(self, args=self.stop_event)
-
-
-    def getCarDirection(self):
-        return (self.carx, self.cary)
-
-    #def getObstaclesDirection(self):
-    #    return (self.obsx, self.obsy)
-
-    #def getAverageDirection(self):
-    #    return (self.avgx, self.avgy)
 
 
     def parse_laser_data(self,laser_data):
