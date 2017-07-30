0201357c4a842e711596803d570bb3b32a07181d
vmartinezf <vanessa_1895@msn.com>
2017-07-30 12:52:26 +0200

    triangulo corregido

--- Test/Vacuum_Practice/gui/widgets/mapWidget.py
+++ Test/Vacuum_Practice/gui/widgets/mapWidget.py
@@ -95,13 +95,20 @@ class MapWidget(QWidget):
         yaw = pose.getYaw()
 
         final_poses = self.RTVacuum() * np.matrix([[x], [y], [1], [1]]) * scale
-        painter.translate(QPoint(final_poses[0], final_poses[1]))
-        painter.rotate(-180*yaw/pi)
-
+        
         triangle = QtGui.QPolygon()
-        triangle.append(QtCore.QPoint(x+50/4, y-50/9))
-        triangle.append(QtCore.QPoint(x+50/4, y+50/3-3))
-        triangle.append(QtCore.QPoint(x-9, y+4.05))
+        triangle.append(QtCore.QPoint(final_poses.flat[0]-50/9, final_poses.flat[1]+50/7))
+        triangle.append(QtCore.QPoint(final_poses.flat[0]+50/3, final_poses.flat[1]-10+50/7))
+        triangle.append(QtCore.QPoint(final_poses.flat[0]+50/3, final_poses.flat[1]+10+50/7))
+        matrix = QtGui.QTransform()
+        matrix.rotate(-180*yaw/pi)
+        triangle = matrix.map(triangle)
+        # The center of triangle is (final_poses.flat[0]+50/9, final_poses.flat[1]+50/7) 
+        center = matrix.map(QtCore.QPoint(final_poses.flat[0]+50/9, final_poses.flat[1]+50/7))
+        xDif = final_poses.flat[0]+50/9 - center.x()
+        yDif = final_poses.flat[1] +50/7- center.y()
+        
+        triangle.translate(xDif, yDif)
 
         pen = QPen(Qt.red, 2)
         painter.setPen(pen)
